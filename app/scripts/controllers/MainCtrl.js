094cc8ba0ff30efcf72c58c5c61bb19480299929
Alvin James <alvinb@zylun.com>
2016-04-16 07:40:00 +0800

    Refactored main controller

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,51 +1,49 @@
 angular
-  .module('signGenerator')
-  .controller('MainController', function($scope, $sce) {
+	.module('signGenerator')
+	.controller('MainController', function($scope, $sce) {
 
-    $scope.remoteImage = "https://ci4.googleusercontent.com/proxy/31CARVhhQT4SewJqVxHUzK3s0LrosHidVlSt4jqF4VlO8KtHnSiWLpwnzY3U7AbC4pLO-PUtx3YtDbHNdimet0ghySjVpd3gXF8=s0-d-e1-ft#http://zylun.com/wp-content/uploads/2014/02/logo.png";
+		var defaultColor = '#505050';
 
-    $scope.details = {
-      name: "",
-      position: "",
-      email: "",
-      number: "",
-      skype: ""
-    };
+		function initDefaults() {
+			$scope.colors = {
+				name: defaultColor,
+				position: defaultColor,
+				email: defaultColor,
+				number: defaultColor,
+				skype: defaultColor
+			};
+		}
 
-		$scope.colors = {
-			name: '#505050',
-			position: '#505050',
-			email: '#505050',
-			number: '#505050',
-			skype: '#505050'
-		};
+		$scope.remoteImage = "http://zylun.com/wp-content/uploads/2014/02/logo.png";
 
-    $scope.isIncomplete = function() {
-      var missing = 0;
-      for(var i in $scope.details) {
-        if(!$scope.details[i]) missing++;
-      }
-      return missing == Object.keys($scope.details).length;
-    }
+		$scope.details = { name: "", position: "", email: "", number: "", skype: "" };
+
+		$scope.isIncomplete = function() {
+			var missing = 0;
+			for(var i in $scope.details) {
+				if(!$scope.details[i]) missing++;
+			}
+			return (missing == Object.keys($scope.details).length);
+		}
 
     $scope.formatHTML = function(label, link, content) {
       return $sce.trustAsHtml(label + ": <a href='"+link+"'>"+content+"</a>");
     };
 
-    $scope.reset = function() {
-      $scope.details = {};
-			for(var i in $scope.colors) {
-				$scope.colors[i] = '#505050';
-			}
-    }
+		$scope.reset = function() {
+			$scope.details = {};
+			initDefaults();
+		}
 
-    $scope.yearCopyright = function() {
-      var d = new Date();
-      return d.getFullYear();
-    }
+		$scope.yearCopyright = function() {
+			var d = new Date();
+			return d.getFullYear();
+		}
 
-    $scope.onSuccess = function(e) {
-      e.clearSelection();
-    }
+		$scope.onSuccess = function(e) {
+			e.clearSelection();
+		}
+
+		initDefaults();
 
   });
